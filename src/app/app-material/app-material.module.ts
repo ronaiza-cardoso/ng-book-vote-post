71af5b813f032571b9954b4db36d1e0db4b874dc
Ronaiza Cardoso <ronaiza.cardoso@basis.com>
2017-08-08 15:19:49 -0300

    Setting upmaterial angular

---
+++ src/app/app-material/app-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MdToolbarModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MdToolbarModule
+  ],
+  declarations: []
+})
+export class AppMaterialModule { }
